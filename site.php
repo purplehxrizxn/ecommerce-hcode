2a7528fc283bc811ae30d41b9adfbd0be0508aca
Panza <gabrielpanzeri@live.com>
2020-10-13 18:00:38 -0300

    added dynamic freight

--- site.php
+++ site.php
@@ -69,9 +69,10 @@ $app->get('/cart', function(){
 	
 	$page = new Page();
 
-	$page->setTPl("cart", [
+	$page->setTpl("cart", [
 		'cart'=>$cart->getValues(),
-		'products'=>$cart->getProducts()
+		'products'=>$cart->getProducts(),
+		'error'=>Cart::getMsgError()
 	]);
 
 });
@@ -121,4 +122,15 @@ $app->get('/cart/:idproduct/remove', function($idproduct) {
 
 	header("Location: /cart");
 	exit;
+});
+
+$app->post('/cart/freight', function (){
+
+	$cart = Cart::getFromSession();
+
+	$cart->setFreight($_POST['zipcode']);
+
+	header("Location /cart");
+	exit;
+
 });
