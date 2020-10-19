e706ba7e77d9b0188e12d35c516413864ccdce77
Panza <gabrielpanzeri@live.com>
2020-10-19 09:04:35 -0300

    added admin password change

--- views-cache/categories-menu.bec7b266bba8520d106c5c9c46b86803.rtpl.php
+++
@@ -1,1 +0,0 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/6">Android</a></li><li><a href="/categories/7">Apple</a></li><li><a href="/categories/8">Celular</a></li><li><a href="/categories/5">Motorola</a></li><li><a href="/categories/4">Samsung</a></li>
